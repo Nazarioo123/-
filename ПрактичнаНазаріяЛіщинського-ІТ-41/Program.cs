e9d808845a2ffac2e4ab551aa7b5caad83ddcb62
Lishchynskiy Nazarii <nazarciklisinskij@gmail.com>
2024-11-02 15:14:32 +0100

    Добавьте файлы проекта.

---
+++ ПрактичнаНазаріяЛіщинського-ІТ-41/Program.cs
@@ -0,0 +1,68 @@
+﻿namespace ПрактичнаНазаріяЛіщинського_ІТ_41
+{
+    using System;
+
+    public class TextEditor
+    {
+        // Властивість для збереження тексту 
+        public string DocumentText { get; private set; } = "";
+
+        // Метод для додавання тексту
+        public void AddText(string txt)
+        {
+            DocumentText += txt;
+        }
+
+        // Метод для друку тексту
+        public void Print(IPrinter printer)
+        {
+            printer.Print(DocumentText);
+        }
+    }
+
+    // Інтерфейс 
+    public interface IPrinter
+    {
+        void Print(string text);
+    }
+
+    // Клас UppercasePrinter друкує текст великими літерами
+    public class UppercasePrinter : IPrinter
+    {
+        public void Print(string text)
+        {
+            Console.WriteLine(text.ToUpper());
+        }
+    }
+
+    
+    public class LowercasePrinter : IPrinter
+    {
+        public void Print(string text)
+        {
+            Console.WriteLine(text.ToLower());
+        }
+    }
+
+    
+    class Program
+    {
+        static void Main()
+        {
+             
+            TextEditor editor = new TextEditor();
+
+            // Додавання тексту
+            editor.AddText("Hello, World!");
+
+            // Друк тексту у верхньому регістрі
+            UppercasePrinter uppercasePrinter = new UppercasePrinter();
+            editor.Print(uppercasePrinter);
+
+            // Друк тексту у нижньому регістрі
+            LowercasePrinter lowercasePrinter = new LowercasePrinter();
+            editor.Print(lowercasePrinter);
+        }
+    }
+
+}
